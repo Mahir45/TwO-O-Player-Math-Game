33cd342c231c5209f264a867a12965d000d35ee8
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2022-01-05 13:28:36 -0500

    final commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+main.rb
+
+require './game'
+require './player'
+require './question'
+
+game = Game.new
+game.
---
+++ player.rb
@@ -0,0 +1,8 @@
+class player
+  attr_accessor :lives, :name
+
+  def initialize(lives, name)
+    @lives = lives
+    @name = name
+  end
+end
